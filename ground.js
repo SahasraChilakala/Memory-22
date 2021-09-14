b655ff91159597f4b61e3a18d87391ca0c9fa0a5
SahasraChilakala <87740974+SahasraChilakala@users.noreply.github.com>
2021-09-14 17:45:34 -0500

    Add files via upload

---
+++ ground.js
@@ -0,0 +1,13 @@
+class Ground {
+    constructor(x,y,width,height){
+        var options = {
+          isStatic: true
+        };
+        this.width = width;
+        this.height = height;
+        this.body = Bodies.rectangle(x, y, this.width, this.height, options);
+        World.add(world, this.body);
+      }
+
+      display();
+}
